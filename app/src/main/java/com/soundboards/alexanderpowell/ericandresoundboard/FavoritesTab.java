e4d5bf8d83196c8afd352ec0f5c08f74b0166dc6
Alexander Powell <ajpowell@email.wm.edu>
2019-10-10 14:18:02 -0700

    new tab added

--- app/src/main/java/com/soundboards/alexanderpowell/ericandresoundboard/FavoritesTab.java
+++ app/src/main/java/com/soundboards/alexanderpowell/ericandresoundboard/FavoritesTab.java
@@ -26,52 +26,19 @@ public class FavoritesTab extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_tab_main, container, false);
+        return inflater.inflate(R.layout.favorites_tab_main, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         sound_file_names = requireActivity().getResources().getStringArray(R.array.sound_file_names);
-
-        MaterialButton shareButton = requireActivity().findViewById(R.id.shareButton);
-        MaterialButton suggestSoundButton = requireActivity().findViewById(R.id.suggestNewSoundButton);
-        MaterialButton randomSoundButton = requireActivity().findViewById(R.id.randomSoundButton);
-
-        shareButton.setOnClickListener(this);
-        suggestSoundButton.setOnClickListener(this);
-        randomSoundButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.shareButton: shareApp(); break;
-            case R.id.suggestNewSoundButton: suggestNewSound(); break;
-            case R.id.randomSoundButton: playRandomSound(); break;
+
         }
-    }
-
-    private void shareApp() {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        String shareBody = getString(R.string.share_app_body);
-        shareBody = shareBody + "\n" + getString(R.string.share_app_url);
-        String shareSubject = getString(R.string.share_subject_text);
-        intent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
-        intent.putExtra(Intent.EXTRA_TEXT, shareBody);
-        startActivity(Intent.createChooser(intent, getString(R.string.share_using_text)));
-    }
-
-    private void suggestNewSound() {
-        Uri uri = Uri.parse(getString(R.string.suggest_new_sound_url));
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
-    }
-
-    private void playRandomSound() {
-        int random = new Random().nextInt(sound_file_names.length);
-        playSound(sound_file_names[random]);
     }
 
     private void playSound(String filename) {
